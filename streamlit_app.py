2af3215e0bac81b576a5b5ae32f42ff8173c429d
ccfsteph <kresics@ccf.org>
2024-12-18 15:19:57 -0500

    Update streamlit_app.py
    Updates for requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,6 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
     #st.write(ingredients_string)
-
-
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + "','" +name_on_order+ """')"""
@@ -47,7 +45,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order + '!', icon
 
-#New section to display smoothiefroot nutrition information
+# New section to display smoothiefroot nutrition information
 import requests
-                   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-                   st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(),user_container_width=True)
